71a2629ce4db58fe7980d91fde9afbf105623513
Tien Dat <tiendatnguyenxuan1607@gmail.com>
2025-04-03 21:01:27 +0700

    ngo dốt

---
+++ src/main/java/com/bookingonline/bookingonlineticket/model/Greeting.java
@@ -0,0 +1,12 @@
+package com.bookingonline.bookingonlineticket.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Greeting {
+
+    private long id;
+    private String content;
+}
