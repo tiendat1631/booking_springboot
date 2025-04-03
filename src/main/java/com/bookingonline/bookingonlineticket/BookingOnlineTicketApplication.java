f6c04ff6c5a892a46105e05d84d1ee7f136bd76f
Tien Dat <tiendatnguyenxuan1607@gmail.com>
2025-04-03 21:01:27 +0700

    initial commit

---
+++ src/main/java/com/bookingonline/bookingonlineticket/BookingOnlineTicketApplication.java
@@ -0,0 +1,13 @@
+package com.bookingonline.bookingonlineticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingOnlineTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingOnlineTicketApplication.class, args);
+	}
+
+}
