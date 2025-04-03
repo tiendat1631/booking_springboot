cc89f3e3f9c5b0ef9fb72bc68a9d8296f703c0d9
NGUYEN VIET ANH KHOA <ITCSIU22278@student.hcmiu.edu.vn>
2025-04-03 23:27:23 +0700

    Update project config

---
+++ src/main/java/org/application/booking/Controller/UserController.java
@@ -0,0 +1,29 @@
+package org.application.booking.Controller;
+
+import org.application.booking.Entity.User;
+import org.application.booking.Repository.UserRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    // Get all users
+    @GetMapping
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    // Create a new user
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+}
