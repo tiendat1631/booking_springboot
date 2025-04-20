a45a0db720123aae8247a410bddc7730334255f0
Tien Dat <tiendatnguyenxuan1607@gmail.com>
2025-04-20 13:56:56 +0700

    commit repository

---
+++ src/main/java/org/application/booking/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package org.application.booking.repository;
+
+
+import org.application.booking.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    // find user by id
+    Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
+}
