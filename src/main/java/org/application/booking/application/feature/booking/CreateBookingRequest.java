fc79947364db5ad6af9f3b09ddc7d0c788bebb11
Tien Dat <tiendatnguyenxuan1607@gmail.com>
2025-07-07 20:23:51 +0700

    update TimeFrame and auto set number of seats BusType

--- src/main/java/org/application/booking/application/feature/booking/CreateBookingRequest.java
+++ src/main/java/org/application/booking/application/feature/booking/CreateBookingRequest.java
@@ -1,6 +1,8 @@
 package org.application.booking.application.feature.booking;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
@@ -8,8 +10,16 @@ import java.util.UUID;
 
 @Getter
 @Setter
+
+@NoArgsConstructor
 public class CreateBookingRequest {
     public UUID tripId;
     public List<UUID> seatIds;
     public UUID userId;
+
+    public <E> CreateBookingRequest(UUID tripId, List<UUID> seatIds, UUID userId) {
+        this.tripId = tripId;
+        this.seatIds = seatIds;
+        this.userId = userId;
+    }
 }
--- src/main/java/org/application/booking/application/service/SearchTripService.java
+++
@@ -1,8 +0,0 @@
-package org.application.booking.application.service;
-
-import lombok.AllArgsConstructor;
-
-@AllArgsConstructor
-public class SearchTripService  {
-
-}
