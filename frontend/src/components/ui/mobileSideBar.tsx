7525c58d4bb8d432c382ed3b078bece4cd8a9595
Tien Dat <tiendatnguyenxuan1607@gmail.com>
2025-07-13 21:50:28 +0700

    add sidebar, fix banner

---
+++ frontend/src/components/ui/mobileSideBar.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import {Button} from "@/components/ui/button";
+import {MenuIcon} from "lucide-react";
+import {Dialog} from "@/components/ui/dialog";
+
+
+export default function MobileSideBar(){
+    const [open, setOpen] = React.useState(false);
+
+    const navItems = [
+        "Đăng nhập/Đăng ký",
+        "Trang chủ",
+        "Lịch trình",
+        "Tra cứu vé",
+        "Tin tức",
+        "Hóa đơn",
+        "Liên hệ",
+        "Về chúng tôi",
+        "Tuyển dụng",
+        "Mạng lưới văn phòng",
+        "Điều khoản sử dụng",
+        "Câu hỏi thường gặp",
+        "Hướng dẫn đặt vé trên Web",
+        "Hướng dẫn nạp tiền trên App",
+    ]
+    return(
+        <div className={"md:hidden"}>
+            {/*nut menu ben trai*/}
+            <Button
+                variant={"ghost"}
+                className={"cursor-pointer"}
+                size={"icon"}
+                onClick={()=>setOpen(true)}>
+                <MenuIcon className={"w-6 h-6 text-white"}/>
+            </Button>
+            {/*sidebar*/}
+            <Dialog open={open} onOpenChange={setOpen}>
+                {open && (
+                    <div className="fixed inset-0 z-50 bg-black/40" onClick={() => setOpen(false)}>
+                        {/* lớp nền mờ, đóng khi click ngoài */}
+                    </div>
+                )}
+
+                {open && (
+                    <div
+                        className="fixed top-0 left-0 z-50 h-full w-[85%] max-w-xs bg-white shadow-lg flex flex-col"
+                    >
+
+                        <nav className="flex flex-col text-sm px-4 text-[12px]">
+                            {navItems.map((item, index) => (
+                                <a
+                                    key={index}
+                                    href="#"
+                                    className={`py-3 border-b text-left ${
+                                        index === 0 ? "text-orange-600 font-semibold " : ""
+                                    }`}
+                                >
+                                    {item}
+                                </a>
+                            ))}
+                        </nav>
+                    </div>
+                )}
+            </Dialog>
+
+        </div>
+    );
+}
