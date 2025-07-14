f352870ea22d4b7279dddad6fbf548703fc0b581
Tien Dat <tiendatnguyenxuan1607@gmail.com>
2025-07-14 19:59:35 +0700

    footer chua xong

--- frontend/src/pages/Homepage/Homepage.tsx
+++ frontend/src/pages/Homepage/Homepage.tsx
@@ -3,6 +3,8 @@ import Header from "@/components/ui/Header.tsx";
 import Banner from "@/components/ui/Banner.tsx";
 import Slider from "@/components/ui/Slider.tsx";
 import PopularRoutes from "@/components/ui/PopularRoutes.tsx";
+import FutaHighlights from "@/components/ui/FutaHighlights.tsx";
+import Footer from "@/components/ui/Footer.tsx";
 export default function Homepage() {
   return (
       <div>
@@ -11,6 +13,8 @@ export default function Homepage() {
         <SearchBox />
           <Slider />
           <PopularRoutes />
+          <FutaHighlights/>
+          <Footer/>
       </div>
 
   )
